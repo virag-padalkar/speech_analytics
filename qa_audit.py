c299f64377e3c7c5b5d8bb2b6230ad5d64db5668
virag.padalkar <virag.padalkar@gmail.com>
2020-11-02 13:53:55 +0800

    defined functions for all modules and tidied up output

--- qa_audit.py
+++ qa_audit.py
@@ -11,16 +11,41 @@ Created on Sat Oct 31 03:52:14 2020
 import speech_recognition as sr
 
 # set up speech regonition object and parse audio file
-r = sr.Recognizer()
-sample_audio = sr.AudioFile("disclaimer_no.wav")
-with sample_audio as source:
-	audio = r.record(source)
-output = r.recognize_google(audio)
-print ("Audio file contents: ")
-print (output)
+def parse_audio():
+	r = sr.Recognizer()
+	sample_audio = sr.AudioFile("disclaimer_yes.wav")
+	with sample_audio as source:
+		audio = r.record(source)
+	output = r.recognize_google(audio)
+	return output
 
-# analyze transciption 
-if "recorded" in output and "monitoring" in output:
-    print ("QA check pass")
-else:
-    print ("QA check fail")
+# print transcript on screen for further processing
+def generate_transcript():
+	transcript = parse_audio()
+	print ("Audio file contents: ")
+	print (transcript)
+	print ("----")
+
+# analyze transciption
+def qa_check():
+    output = parse_audio()
+    if "recorded" and "monitoring" in output:
+        print ("Recording disclaimer pass")
+        print ("----")
+    else:
+        print ("Recording disclaimer fail")
+        print ("----")
+    
+    if "prices" and "subject" and "change" in output:
+        print ("Pricing disclaimer pass")
+        print ("----")
+    else:
+        print ("Pricing disclaimer fail")
+        print ("----")
+
+def main():
+	generate_transcript()
+	qa_check()
+	
+if __name__ == "__main__":
+	main()
